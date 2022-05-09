c403f16ff6505b73bd4e75da8753f10c45592103
Miljan Puletic <miljan.puletic@gmail.com>
2022-05-09 09:25:00 +0200

    added services for user-service

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
-INSERT INTO `scheduler`.`users` (`user_id`, `firstname`,`lastname`,`email`,`password`,`username`, `role`) VALUES (1, 'Petar', 'Petrovic', 'petarp@gmail.com', 'a', 'pera', 'ADMIN');
-INSERT INTO `scheduler`.`users` (`user_id`, `firstname`,`lastname`,`email`,`password`,`username`, `role`) VALUES (1, 'Aleksandar', 'Aleksic', 'aleksa@gmail.com', 'b', 'alek', 'USER');
+INSERT INTO `users`.`users` (`user_id`, `firstname`,`lastname`,`email`,`password`,`username`, `role`)  VALUES (1, 'Petar', 'Petrovic', 'petarp@gmail.com', 'a', 'pera', 'ADMIN');
+INSERT INTO `users`.`users` (`user_id`, `firstname`,`lastname`,`email`,`password`,`username`, `role`)  VALUES (2, 'Aleksandar', 'Aleksic', 'aleksa@gmail.com', 'b', 'alek', 'USER');
+INSERT INTO `users`.`users` (`user_id`, `firstname`,`lastname`,`email`,`password`,`username`, `role`)  VALUES (3, 'Mitar', 'Miric', 'mita@gmail.com', 'm', 'mita', 'USER');
